e7f0dc1b9e5a0977aa6483594079a9f977f35e9a
Tushar Goyal <tushar462024@gmail.com>
2022-05-27 03:19:35 +0530

    Trying to configure null-ls to make it work like coc

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local M = {}
 M.setup_lsp = function(attach, capabilities)
    local lspconfig = require "lspconfig"
 
-   local servers = { "html", "cssls", "bashls", "emmet_ls", "tsserver", "clangd" }
+   local servers = { "cssls", "dartls", "dockerls", "dotls", "emmet_ls", "eslint", "grammarly", "graphql", "html", "jsonls", "prismals", "pyright", "stylelint_lsp", "sumneko_lua", "svelte", "tailwindcss", "tsserver", "vimls", "vuels", "yamlls" }
 
    for _, lsp in ipairs(servers) do
       lspconfig[lsp].setup {
--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -1,29 +1,41 @@
-local present, null_ls = pcall(require, "null-ls")
-
-if not present then
-   return
-end
-
+local null_ls = require "null-ls"
 local b = null_ls.builtins
 
 local sources = {
 
-   -- webdev stuff
-   b.formatting.deno_fmt,
-   b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } },
+   b.code_actions.eslint,
+   b.code_actions.gitsigns,
+   b.formatting.fixjson,
+   b.formatting.prettier,
+   b.completion.luasnip,
+   b.completion.spell,
+   b.diagnostics.write_good,
+   b.diagnostics.eslint,
+   b.diagnostics.tsc,
 
    -- Lua
    b.formatting.stylua,
+   b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
 
    -- Shell
    b.formatting.shfmt,
    b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
-
-   -- cpp
-   b.formatting.clang_format,
 }
 
-null_ls.setup {
-   debug = true,
-   sources = sources,
-}
+local M = {}
+
+M.setup = function()
+   null_ls.setup {
+      debug = true,
+      sources = sources,
+
+      -- format on save
+      on_attach = function(client)
+         if client.resolved_capabilities.document_formatting then
+            vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+         end
+      end,
+   }
+end
+
+return M
